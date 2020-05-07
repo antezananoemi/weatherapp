2c04abca6ee62b1a6e29f7bef60b59e746bfd586
Noemi Antezana <antezananoemi@yahoo.com>
2020-05-06 21:45:15 -0400

    validate lat lon on endpoint

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -7,42 +7,51 @@ const cacheFile = config.get("cache.file");
 const weatherService = require("../../services/weather.service");
 const mapboxService = require("../../services/mapbox.service");
 
+const checkLatLon = (lat, lon) => {
+  const regex = /^(\-?([0-8]?[0-9](\.\d+)?|90(.[0]+)?)\s?[,]\s?)+(\-?([1]?[0-7]?[0-9](\.\d+)?|180((.[0]+)?)))$/;
+  return regex.test(`${lat},${lon}`);
+};
+
 // @route GET api/weather
 // @desc Get actual weather
 // @access Public
 router.get("/:lat/:lon", async (req, res) => {
-  //TODO add city name
   let { lat, lon } = req.params;
-  const place = await mapboxService.getCity(lat, lon);
-  let rawdata = fs.readFileSync(config.cache.file);
-  let weatherResult =
-    Object.keys(rawdata).length !== 0 ? JSON.parse(rawdata) : [];
-
-  const onCache = weatherResult.find((o) => {
-    return (
-      o.coord.lon == parseFloat(lon).toFixed(2) &&
-      o.coord.lat == parseFloat(lat).toFixed(2)
-    );
-  });
-
-  if (!onCache) {
-    const weatherParams = {
-      ...req.params,
-      apiUrl,
-    };
-    weatherService
-      .getWeather(weatherParams)
-      .then((result) => {
-        weatherResult.push({ ...result, place });
-        fs.writeFileSync(cacheFile, JSON.stringify(weatherResult));
-        return res.send({ ...result, place });
-      })
-      .catch((err) => {
-        res.status(500).send(err);
-      });
+  if (!checkLatLon(lat, lon)) {
+    res.status(422).send({
+      err: "Please provide correct values for latitude and longitude",
+    });
   } else {
-    console.log("desde cache..");
-    return res.send(onCache);
+    let rawdata = fs.readFileSync(config.cache.file);
+    let weatherResult =
+      Object.keys(rawdata).length !== 0 ? JSON.parse(rawdata) : [];
+    const onCache = weatherResult.find((o) => {
+      return (
+        o.coord.lon == parseFloat(lon).toFixed(2) &&
+        o.coord.lat == parseFloat(lat).toFixed(2)
+      );
+    });
+
+    if (!onCache) {
+      const place = await mapboxService.getCity(lat, lon);
+      const weatherParams = {
+        ...req.params,
+        apiUrl,
+      };
+      weatherService
+        .getWeather(weatherParams)
+        .then((result) => {
+          weatherResult.push({ ...result, place });
+          fs.writeFileSync(cacheFile, JSON.stringify(weatherResult));
+          return res.send({ ...result, place });
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    } else {
+      console.log("desde cache..");
+      return res.send(onCache);
+    }
   }
 });
 
